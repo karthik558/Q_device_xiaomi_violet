0e08a4784e88f4e19839cef8e612a7d81f6b19a2
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-15 20:06:19 +0200

    violet: add an overlay to specify notch presence
    Change-Id: Ib0e29c580c384be6f0d9c6d20d8f382fa2971408

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product-if-exists, vendor/xiaomi/violet/violet-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml \
