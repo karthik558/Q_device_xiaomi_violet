d94608e464b1547d9fa2e841cd128fe778c8d763
Karthik SP <53151501+karthik558@users.noreply.github.com>
2020-08-12 01:59:47 +0530

    Update Coral build august fingerprint
    It will pass safety net without magisk.

--- havoc_violet.mk
+++ havoc_violet.mk
@@ -22,7 +22,7 @@ export CHERISH_BUILD_TYPE:= OFFICIAL
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.cherish.maintainer=Karthik Lal
 
-BUILD_FINGERPRINT := "google/flame/flame:10/QQ3A.200705.002/6506677:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V11.0.8.0.PFHINXM release-keys" \
