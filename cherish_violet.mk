32cc8a77002ff5c44d59ce36c36db387181ad13f
Karthik SP <53151501+karthik558@users.noreply.github.com>
2020-09-11 14:32:39 +0530

    Update build fingerprint to Coral Sept Release

--- cherish_violet.mk
+++ cherish_violet.mk
@@ -22,14 +22,14 @@ export CHERISH_BUILD_TYPE:= OFFICIAL
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.cherish.maintainer=Karthik Lal
 
-BUILD_FINGERPRINT := "google/flame/flame:10/QQ3A.200805.001/6578210:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V11.0.8.0.PFHINXM release-keys" \
     PRODUCT_NAME="violet"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.build.fingerprint=google/flame/flame:10/QQ3A.200805.001/6578210:user/release-keys
+ro.build.fingerprint=google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
