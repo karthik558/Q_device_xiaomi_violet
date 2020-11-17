14b1f0305e736045591a1c0fe1fdd28025f1dc26
Karthik SP <karthik.lal558@gmail.com>
2020-11-17 07:49:03 +0000

    violet:Use Redfin November FP

--- ancient_violet.mk
+++ ancient_violet.mk
@@ -21,14 +21,14 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.ancient.maintainer=Karthik Lal
 
-BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V11.0.8.0.PFHINXM release-keys" \
     PRODUCT_NAME="violet"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.build.fingerprint=google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
+ro.build.fingerprint=google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
